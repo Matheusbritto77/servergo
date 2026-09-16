5f8635c882dc934ee679519fab24aecdd94b68ed
matheusbrito <matheusbrito@MacBook-Air-de-matheusbrito.local>
2026-09-15 22:48:39 -0300

    Boost throughput: 25 Mbps bitrate and 120 FPS high capacity video stream

--- broker/broker.go
+++ broker/broker.go
@@ -232,7 +232,7 @@ func (b *Broker) VideoStream(stream pb.RemoteDesktop_VideoStreamServer) error {
 		}
 
 		if ch == nil && registeredID != "" {
-			ch = make(chan *pb.VideoFrame, 4)
+			ch = make(chan *pb.VideoFrame, 8)
 			if client, exists := b.findClient(registeredID); exists {
 				client.videoSubMutex.Lock()
 				client.videoSubscribers[ch] = true
